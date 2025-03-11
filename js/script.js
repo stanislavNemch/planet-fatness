e2754c470c5e7a554900d2e6cacb510b28095f86
Stanislav Nemchenko <stas.buka@gmail.com>
2025-03-11 02:30:32 +0200

    add modal-overlay to Request a callback

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showForm() {
+    document.getElementById("modal-overlay").style.display = "block";
+    document.querySelector(".overlay").style.display = "block";
+}
+
+function hideForm() {
+    document.getElementById("modal-overlay").style.display = "none";
+    document.querySelector(".overlay").style.display = "none";
+}
